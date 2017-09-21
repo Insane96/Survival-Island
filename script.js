e4111b112030e3695c52c5c4a2f9820b0e8ad3a3
Alberto Del Villano <insane96mcp@gmail.com>
2017-09-21 08:50:00 +0200

    Day 2 w/ fixed collision detection

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ var Game = {
 		Game.oldTime = newTime;
 		
 		for (var i = 0; i < Game.GameObjects.length; i++){
-			Game.GameObjects[i].Update();
+			if (typeof Game.GameObjects[i].Update === "function")
+				Game.GameObjects[i].Update();
 		}
 	},
 	
@@ -31,44 +32,101 @@ window.onload = function(){
 	window.setInterval(function(){
 		Game.Update();
 	}, 1000 / 60);
+	
+	
+	island = new Island(100, 100, 750, 450);
 }
 
 window.addEventListener("keydown", function(e){
 	for (var i = 0; i < Game.GameObjects.length; i++){
-		Game.GameObjects[i].OnKeyDown(e);
+		if (typeof Game.GameObjects[i].OnKeyDown === "function")
+			Game.GameObjects[i].OnKeyDown(e);
 	}
 });
 
 window.addEventListener("keypress", function(e){
 	for (var i = 0; i < Game.GameObjects.length; i++){
-		Game.GameObjects[i].OnKeyPress(e);
+		if (typeof Game.GameObjects[i].OnKeyPress === "function")
+			Game.GameObjects[i].OnKeyPress(e);
 	}
 });
 
 window.addEventListener("keyup", function(e){
 	for (var i = 0; i < Game.GameObjects.length; i++){
-		Game.GameObjects[i].OnKeyUp(e);
+		if (typeof Game.GameObjects[i].OnKeyUp === "function")
+			Game.GameObjects[i].OnKeyUp(e);
 	}
 });
 
-function Player(name){
-	this.pos = new Vector2(0, 0);
+function KeyMap(up, down, left, right, action){
+	if (typeof up == "string")
+		up = up.toUpperCase().charCodeAt(0);
+	if (typeof down == "string")
+		down = down.toUpperCase().charCodeAt(0);
+	if (typeof left == "string")
+		left = left.toUpperCase().charCodeAt(0);
+	if (typeof right == "string")
+		right = right.toUpperCase().charCodeAt(0);
+	if (typeof action == "string")
+		action = action.toUpperCase().charCodeAt(0);
+	
+	this.up = up;
+	this.down = down;
+	this.left = left;
+	this.right = right;
+	this.action = action;
+}
+
+var player;
+
+function Player(pos, name, keymap){
+	if (pos.x == null)
+		pos = Vector2.Zero;
+	this.pos = pos;
+	this.size = new Vector2(30, 30)
 	this.name = name;
+	if (keymap === undefined)
+		keymap = new KeyMap(KeyCodes.ArrowUp, KeyCodes.ArrowDown, KeyCodes.ArrowLeft, KeyCodes.ArrowRight, KeyCodes.Space);
+	
+	this.keymap = keymap;
+	
 	this.dom = document.createElement("div");
+	this.dom.className = "player";
+	
 	this.dom.style.position = "absolute";
-	this.dom.style.width = "30px";
-	this.dom.style.height = "30px";
-	this.dom.style.backgroundColor = "#0f0";
+	this.dom.style.width = this.size.x;
+	this.dom.style.height = this.size.y;
+	this.dom.style.left = this.pos.x + "px";
+	this.dom.style.top = this.pos.y + "px";
 	
 	this.direction = Vector2.Zero;
-	this.movementSpeed = 20;
+	this.movementSpeed = 50;
 	
 	this.Update = function(){
-		Debug.Write("Directions: [" + this.direction.x + "," + this.direction.y + "]");
+		Debug.Write("Direction: [" + this.direction.x + "," + this.direction.y + "]");
 		
 		var normDirection = Vector2.Normalize(this.direction);
 		var speed = Vector2.Multiply(normDirection, this.movementSpeed * Game.deltaTime);
+		var oldPos = this.pos;
 		this.pos = Vector2.Sum(this.pos, speed);
+		
+		if (this.pos.x < island.pos.x)
+			this.pos.x = island.pos.x;
+		if (this.pos.y < island.pos.y)
+			this.pos.y = island.pos.y
+		if (this.pos.x + this.size.x > island.pos.x + island.size.x)
+			this.pos.x = island.pos.x + island.size.x - this.size.x;
+		if (this.pos.y + this.size.y > island.pos.y + island.size.y)
+			this.pos.y = island.pos.y + island.size.y - this.size.y;
+		
+		for (var i = 0; i < island.rocks.length; i++){
+			var rock = island.rocks[i];
+			if (this.pos.x < rock.pos.x + rock.size.x && 
+				this.pos.x + this.size.x > rock.pos.x && 
+				this.pos.y < rock.pos.y + rock.size.y && 
+				this.pos.y + this.size.y > rock.pos.y)
+				this.pos = oldPos;
+		}
 		
 		this.dom.style.left = this.pos.x + "px";
 		this.dom.style.top = this.pos.y + "px";
@@ -77,18 +135,22 @@ function Player(name){
 	
 	this.OnKeyDown = function(event){
 		console.log("down: " + event.keyCode);
-		if (event.keyCode == KeyCodes.ArrowLeft && this.direction.x == 0){
-			this.direction = Vector2.Sum(this.direction, Vector2.Left);
+		if (this.direction.x == 0){
+			if (event.keyCode == this.keymap.left)
+				this.direction = Vector2.Sum(this.direction, Vector2.Left);
+			
+			if (event.keyCode == this.keymap.right)
+				this.direction = Vector2.Sum(this.direction, Vector2.Right);
 		}
-		if (event.keyCode == KeyCodes.ArrowRight && this.direction.x == 0){
-			this.direction = Vector2.Sum(this.direction, Vector2.Right);
+		
+		if (this.direction.y == 0){
+			if (event.keyCode == this.keymap.up)
+				this.direction = Vector2.Sum(this.direction, Vector2.Up);
+			
+			if (event.keyCode == this.keymap.down)
+				this.direction = Vector2.Sum(this.direction, Vector2.Down);
 		}
-		if (event.keyCode == KeyCodes.ArrowUp && this.direction.y == 0){
-			this.direction = Vector2.Sum(this.direction, Vector2.Up);
-		}
-		if (event.keyCode == KeyCodes.ArrowDown && this.direction.y == 0){
-			this.direction = Vector2.Sum(this.direction, Vector2.Down);
-		}
+		
 	}
 	
 	this.OnKeyPress = function(event){
@@ -96,17 +158,16 @@ function Player(name){
 	}
 	
 	this.OnKeyUp = function(event){
-		console.log("up: " + event.keyCode);
-		if (event.keyCode == KeyCodes.ArrowLeft && this.direction.x == -1){
+		if (event.keyCode == this.keymap.left && this.direction.x == -1){
 			this.direction = Vector2.Subtract(this.direction, Vector2.Left);
 		}
-		if (event.keyCode == KeyCodes.ArrowRight && this.direction.x == 1){
+		if (event.keyCode == this.keymap.right && this.direction.x == 1){
 			this.direction = Vector2.Subtract(this.direction, Vector2.Right);
 		}
-		if (event.keyCode == KeyCodes.ArrowUp && this.direction.y == -1){
+		if (event.keyCode == this.keymap.up && this.direction.y == -1){
 			this.direction = Vector2.Subtract(this.direction, Vector2.Up);
 		}
-		if (event.keyCode == KeyCodes.ArrowDown && this.direction.y == 1){
+		if (event.keyCode == this.keymap.down && this.direction.y == 1){
 			this.direction = Vector2.Subtract(this.direction, Vector2.Down);
 		}
 	}
@@ -114,4 +175,50 @@ function Player(name){
 	document.body.appendChild(this.dom);
 	
 	Game.GameObjects.push(this);
+}
+
+var island;
+
+function Island(x, y, width, height){
+	this.pos = new Vector2(x, y);
+	this.size = new Vector2(width, height);
+	
+	this.dom = document.createElement("div");
+	this.dom.id = "island";
+	
+	this.dom.style.position = "absolute";
+	this.dom.style.width = this.size.x;
+	this.dom.style.height = this.size.y;
+	this.dom.style.left = this.pos.x + "px";
+	this.dom.style.top = this.pos.y + "px";
+	this.dom.style.zIndex = -1;
+	
+	this.rocks = [];
+	
+	player = new Player(this.pos, "name");
+	for (var i = 0; i < 10; i++){
+		var rockSize = new Vector2(Math.random() * 10 + 15, Math.random() * 10 + 15)
+		var rock = new Rock(Math.random() * (this.size.x - rockSize.x) + this.pos.x, Math.random() * (this.size.y - rockSize.y) + this.pos.y, rockSize.x, rockSize.y);
+		this.rocks.push(rock);
+	}
+	
+	document.body.appendChild(this.dom);
+}
+
+function Rock(x, y, width, height){
+	this.pos = new Vector2(x, y);
+	this.size= new Vector2(width, height);
+	
+	this.dom = document.createElement("div");
+	this.dom.className = "rock";
+	
+	this.dom.style.position = "absolute";
+	this.dom.style.width = this.size.x;
+	this.dom.style.height = this.size.y;
+	this.dom.style.left = this.pos.x + "px";
+	this.dom.style.top = this.pos.y + "px";
+	
+	Game.GameObjects.push(this);
+	
+	document.body.appendChild(this.dom);
 }
